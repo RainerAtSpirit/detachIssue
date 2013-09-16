e21fab548d5921a64fc9fd2c550621bc8a69dfc5
RainerAtSpirit <R.Wittmann@spirit.de>
2013-09-16 10:39:32 +0200

    add canActivate/canDeactivate to ko/animatedTrans

--- app/ko/animatedTrans/index.js
+++ app/ko/animatedTrans/index.js
@@ -1,13 +1,13 @@
-﻿define(['jquery', 'knockout'], function ($, ko) {
+﻿define(['jquery', 'knockout', 'durandal/app'], function( $, ko, app ) {
     // Here's a custom Knockout binding that makes elements shown/hidden via jQuery's fadeIn()/fadeOut() methods
     // Could be stored in a separate utility library
     ko.bindingHandlers.fadeVisible = {
-        init: function (element, valueAccessor) {
+        init: function( element, valueAccessor ) {
             // Initially set the element to be instantly visible/hidden depending on the value
             var value = valueAccessor();
             $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
         },
-        update: function (element, valueAccessor) {
+        update: function( element, valueAccessor ) {
             // Whenever the value subsequently changes, slowly fade the element in or out
             var value = valueAccessor();
             ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
@@ -30,25 +30,37 @@
 
     var displayAdvancedOptions = ko.observable(false);
 
-    var addPlanet = function (type) {
+    var addPlanet = function( type ) {
         planets.push({
             name: "New planet",
             type: type
         });
     };
 
-    var planetsToShow = ko.computed(function () {
+    var planetsToShow = ko.computed(function() {
         // Represents a filtered list of planets
         // i.e., only those matching the "typeToShow" condition
         var desiredType = typeToShow();
-        if (desiredType == "all") return planets();
-        return ko.utils.arrayFilter(planets(), function (planet) {
+        if ( desiredType == "all" ) {
+            return planets();
+        }
+        return ko.utils.arrayFilter(planets(), function( planet ) {
             return planet.type == desiredType;
         });
     });
 
-    var showPlanetElement = function (elem) { if (elem.nodeType === 1) $(elem).hide().slideDown() }
-    var hidePlanetElement = function (elem) { if (elem.nodeType === 1) $(elem).slideUp(function () { $(elem).remove(); }) }
+    var showPlanetElement = function( elem ) {
+        if ( elem.nodeType === 1 ) {
+            $(elem).hide().slideDown()
+        }
+    }
+    var hidePlanetElement = function( elem ) {
+        if ( elem.nodeType === 1 ) {
+            $(elem).slideUp(function() {
+                $(elem).remove();
+            })
+        }
+    }
 
     return {
         planets: planets,
@@ -57,6 +69,13 @@
         addPlanet: addPlanet,
         planetsToShow: planetsToShow,
         showPlanetElement: showPlanetElement,
-        hidePlanetElement: hidePlanetElement
+        hidePlanetElement: hidePlanetElement,
+        canActivate: function() {
+            return app.showMessage('Do you want to proceed?', 'canActivate', ['Yes', 'No']);
+        },
+
+        canDeactivate: function() {
+            return app.showMessage('Do you want to leave?', 'canDeactivate', ['Yes', 'No']);
+        }
     }
 });
